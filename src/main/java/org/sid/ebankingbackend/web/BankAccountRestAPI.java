523f44265c136f5f94e34a0763f39ddeb76d5fb7
khoumani asmaa <khoumaniasmaa@gmail.com>
2022-06-25 02:09:44 +0200

    3eme commit

--- src/main/java/org/sid/ebankingbackend/web/BankAccountRestAPI.java
+++ src/main/java/org/sid/ebankingbackend/web/BankAccountRestAPI.java
@@ -1,25 +1,24 @@
 package org.sid.ebankingbackend.web;
 
-import org.sid.ebankingbackend.dtos.AccountHistoryDTO;
-import org.sid.ebankingbackend.dtos.AccountOperationDTO;
-import org.sid.ebankingbackend.dtos.BankAccountDTO;
+import org.sid.ebankingbackend.dtos.*;
 import org.sid.ebankingbackend.exceptions.BankAccountNotFoundException;
+import org.sid.ebankingbackend.exceptions.BanlanceNotSufficientException;
 import org.sid.ebankingbackend.services.BankAccountService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class BankAccountRestAPI {
-    private  BankAccountService bankAccountService;
-    public BankAccountRestAPI(BankAccountService bankAccountService){
-        this.bankAccountService =bankAccountService;
+    private BankAccountService bankAccountService;
+
+    public BankAccountRestAPI(BankAccountService bankAccountService) {
+        this.bankAccountService = bankAccountService;
     }
+
     @GetMapping("/accounts/{accountId}")
-    public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException, BankAccountNotFoundException {
+    public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
         return bankAccountService.getBankAccount(accountId);
     }
     @GetMapping("/accounts")
@@ -30,11 +29,29 @@ public class BankAccountRestAPI {
     public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
         return bankAccountService.accountHistory(accountId);
     }
+
     @GetMapping("/accounts/{accountId}/pageOperations")
     public AccountHistoryDTO getAccountHistory(
             @PathVariable String accountId,
             @RequestParam(name="page",defaultValue = "0") int page,
-            @RequestParam(name="size",defaultValue = "5")int size){
+            @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
         return bankAccountService.getAccountHistory(accountId,page,size);
+    }
+    @PostMapping("/accounts/debit")
+    public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BanlanceNotSufficientException {
+        this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
+        return debitDTO;
+    }
+    @PostMapping("/accounts/credit")
+    public CreditDTO credit(@RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException {
+        this.bankAccountService.credit(creditDTO.getAccountId(),creditDTO.getAmount(),creditDTO.getDescription());
+        return creditDTO;
+    }
+    @PostMapping("/accounts/transfer")
+    public void transfer(@RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BanlanceNotSufficientException {
+        this.bankAccountService.transfer(
+                transferRequestDTO.getAccountSource(),
+                transferRequestDTO.getAccountDestination(),
+                transferRequestDTO.getAmount());
     }
 }
--- src/main/java/org/sid/ebankingbackend/web/CustomerRestController.java
+++ src/main/java/org/sid/ebankingbackend/web/CustomerRestController.java
@@ -13,6 +13,8 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin("*")
+
 public class CustomerRestController {
     private BankAccountService bankAccountService;
     @GetMapping("/customers")
@@ -20,6 +22,11 @@ public List<CustomerDTO> customers(){
 
         return bankAccountService.listCustomer();
 }
+    @GetMapping("/customers/search")
+    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword)
+    {
+        return bankAccountService.searchCustomers("%"+keyword+"%");
+    }
 @GetMapping("/customers/{id}")
     public CustomerDTO getCustomer (@PathVariable (name = "id") Long customerId)throws CustomerNotFoundException {
         return  bankAccountService.getCustomer(customerId);
